81af0ba4df34566650a41f7061eba6fc08fcb984
fabiobragato <fabio_bragato@hotmail.com>
2021-11-24 09:55:03 -0300

    more corrections

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import find_packages, setup
 setup(
     name='datascience',
     packages=find_packages(include=['dataprocessing_fb', 'datafabric_fb']),
-    version='0.3.1',
+    version='0.3.2',
     description='Tools for Data Science',
     author='Fabio Bragato',
     author_email='fabio_bragato@hotmail.com',
     license='MIT',
-    install_requires=['pandas', 'numpy', 'boto3', 'pyarrow', 'random']
+    install_requires=['pandas', 'numpy', 'boto3', 'pyarrow']
 )
